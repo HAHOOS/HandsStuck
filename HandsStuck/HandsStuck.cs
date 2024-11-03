bc197da55c3181507a0d67199a019125c6bae370
HAHOOS <hahoos@hahoos.pl>
2024-11-03 21:58:03 +0100

    Update version

--- HandsStuck/HandsStuck.cs
+++ HandsStuck/HandsStuck.cs
@@ -6,7 +6,7 @@ namespace HandsStuck
 {
     public class HandsStuck : MelonMod
     {
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.1";
 
         public override void OnInitializeMelon()
         {
