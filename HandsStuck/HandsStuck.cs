48c7a2e6c068063b4ef63ba132bfaa30b94877d1
HAHOOS <hahoos@hahoos.pl>
2024-11-03 19:57:17 +0100

    Add project files.

---
+++ HandsStuck/HandsStuck.cs
@@ -0,0 +1,26 @@
+﻿using MelonLoader;
+using System.Linq;
+using UnityEngine.XR.OpenXR;
+
+namespace HandsStuck
+{
+    public class HandsStuck : MelonMod
+    {
+        public const string Version = "1.0.0";
+
+        public override void OnInitializeMelon()
+        {
+            var eyegaze = OpenXRSettings.Instance.features.First(x => x.name == "EyeGazeInteraction Standalone");
+            if (eyegaze?.enabled == true)
+            {
+                eyegaze.enabled = false;
+                OpenXRSettings.Instance.ApplySettings();
+                LoggerInstance.Msg("Disabled EyeGazeInteraction feature, which breaks controller tracking on SteamVR beta");
+            }
+            else
+            {
+                LoggerInstance.Msg("It seems like the feature is not enabled, which means the bug was already fixed by the devs");
+            }
+        }
+    }
+}
---
+++ HandsStuck/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using HandsStuck;
+using MelonLoader;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#region MelonLoader
+
+[assembly: MelonInfo(typeof(HandsStuck.HandsStuck), "HandsStuck", "1.0.0", "HAHOOS", null)]
+[assembly: MelonGame("Stress Level Zero", "BONELAB")]
+[assembly: MelonColor(0, 191, 35, 0)]
+
+#endregion MelonLoader
+
+#region General
+
+[assembly: AssemblyTitle("Fixes the controller tracking bug in newer versions of SteamVR")]
+[assembly: AssemblyDescription("Fixes the controller tracking bug in newer versions of SteamVR")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HAHOOS")]
+[assembly: AssemblyProduct("HandsStuck")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion(HandsStuck.HandsStuck.Version)]
+[assembly: AssemblyFileVersion(HandsStuck.HandsStuck.Version)]
+[assembly: AssemblyInformationalVersion(HandsStuck.HandsStuck.Version)]
+
+#endregion General
+
+#region Other
+
+[assembly: ComVisible(false)]
+[assembly: Guid("5293f777-bfbf-4a4d-a006-95a2fc2670c3")]
+
+#endregion Other
